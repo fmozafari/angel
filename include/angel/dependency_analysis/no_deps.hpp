a0e6e68f928c816a76cfeba754653f569f6bce6c
Fereshte Mozafari <fereshte.mozafari@epfl.ch>
2020-03-13 11:43:53 +0100

    add dependency analysis method

---
+++ include/angel/dependency_analysis/no_deps.hpp
@@ -0,0 +1,55 @@
+#include <map>
+#include <fmt/format.h>
+#include <iostream>
+#include <angel/quantum_state_preparation/qsp_tt_general.hpp>
+
+namespace angel
+{
+
+class NoDeps
+{
+public:
+    using dependencies_t = std::map<uint32_t, std::vector<std::pair<std::string, std::vector<uint32_t>>>>;
+
+public:
+    void print_dependencies(dependencies_t const &dependencies, std::ostream &os = std::cout)
+    {
+        os << "[i] dependencies:" << std::endl;
+        os << "dependencies size: " << dependencies.size() << std::endl;
+        for (const auto &d : dependencies)
+        {
+            os << d.first << "  ";
+            for (const auto &dd : d.second)
+            {
+                os << dd.first << ' ';
+                for (const auto &c : dd.second)
+                {
+                    os << c << ' ';
+                }
+            }
+            os << std::endl;
+        }
+    }
+
+public:
+    explicit NoDeps()
+    {
+    }
+
+    dependencies_t run(kitty::dynamic_truth_table const &tt, qsp_general_stats &stats)
+    {
+        stats.total_bench++;
+        dependencies_t deps;
+        return deps;
+    }
+
+    bool get_considering_deps()
+    {
+        return considering_deps;
+    }
+
+protected:
+    bool considering_deps = false;
+}; /// class NoDeps end
+
+} /// namespace angel
