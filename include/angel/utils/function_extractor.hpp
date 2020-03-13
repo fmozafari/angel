7bfe7eb0e4d04c37c63dc7f6e457afe33e238b01
Heinz Riener <heinz.riener@gmail.com>
2020-03-13 21:45:06 +0100

    `function_extractor` to extract Boolean functions from an AIGER file.

---
+++ include/angel/utils/function_extractor.hpp
@@ -0,0 +1,98 @@
+/* angel
+ * Copyright (C) 2019  EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*!
+  \file function_extractor.hpp
+  \brief Function extractor
+
+  \author Heinz Riener
+*/
+
+#pragma once
+
+#include <mockturtle/mockturtle.hpp>
+#include <lorina/lorina.hpp>
+#include <kitty/kitty.hpp>
+
+namespace angel
+{
+
+struct function_extractor_params
+{
+  uint32_t num_vars = 6u;
+}; /* function_extractor_params */
+
+class function_extractor
+{
+public:
+  explicit function_extractor( std::string const& filename, function_extractor_params ps = {} )
+    : filename( filename )
+    , ps( ps )
+  {
+  }
+
+  bool parse()
+  {
+    lorina::diagnostic_engine diag;
+    return ( lorina::read_aiger( filename, mockturtle::aiger_reader( aig ), &diag ) == lorina::return_code::success );
+  }
+
+  template<typename Fn>
+  void run( Fn&& fn )
+  {
+    mockturtle::depth_view depth_aig( aig );
+    mockturtle::fanout_view fanout_aig( depth_aig );
+
+    mockturtle::cut_manager<decltype( fanout_aig )> mgr( ps.num_vars );
+    fanout_aig.foreach_gate( [&]( const auto& n ){
+        auto const leaves = mockturtle::reconv_driven_cut( mgr, fanout_aig, n );
+
+        mockturtle::cut_view cut{fanout_aig, leaves, aig.make_signal( n )};
+
+        mockturtle::default_simulator<kitty::dynamic_truth_table> sim( leaves.size() );
+        auto const result = mockturtle::simulate_nodes<kitty::dynamic_truth_table>( cut, sim );
+
+        cut.foreach_po( [&]( const auto& s ){
+            auto const tt = result[cut.get_node( s )];
+
+            auto const it = tts.find( tt );
+            if ( it == std::end( tts ) )
+            {
+              tts.insert( tt );
+              fn( tt );
+            }
+          });
+      });
+  }
+
+protected:
+  std::string filename;
+  function_extractor_params ps;
+
+  mockturtle::aig_network aig;
+  std::set<kitty::dynamic_truth_table> tts;
+}; /* function_extractor */
+
+} /* angel */
