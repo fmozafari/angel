09827929b95f08144af82b8bf09e017d9fbc9e5a
Fereshte Mozafari <fereshte.mozafari@epfl.ch>
2020-03-14 10:27:24 +0100

    add parameter for random ordering+change arguments of qsp_tt_general

--- include/angel/reordering/random_ordering.hpp
+++ include/angel/reordering/random_ordering.hpp
@@ -9,8 +9,11 @@ namespace angel
 
 class RandomOrdering
 {
+private:
+uint32_t num = 1;
 public:
     explicit RandomOrdering() {}
+    explicit RandomOrdering(uint32_t n): num(n) {}
 
     std::vector<std::vector<uint32_t>> run(uint32_t var_count)
     {
@@ -19,10 +22,18 @@ public:
         for(auto i=0u; i<var_count; i++)
             orders_init.emplace_back(i);
 
-        std::random_device rd;
-        std::mt19937 g(rd());
-        std::shuffle(orders_init.begin(), orders_init.end(), g);
-        all_orders.emplace_back(orders_init);
+        // std::random_device rd;
+        // std::mt19937 g(rd());
+        // obtain a time-based seed:
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::default_random_engine e(seed);
+
+        for(auto i=0u; i<num; i++)
+        {
+            std::shuffle(orders_init.begin(), orders_init.end(), e);
+            all_orders.emplace_back(orders_init);
+        }
+        
 
         return all_orders;
     }
