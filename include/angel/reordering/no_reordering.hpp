2ee23abecd892dc1be4695eb1f58b6beba596529
Fereshte Mozafari <fereshte.mozafari@epfl.ch>
2020-03-15 11:47:49 +0100

    reverse no reordering

--- include/angel/reordering/no_reordering.hpp
+++ include/angel/reordering/no_reordering.hpp
@@ -9,22 +9,23 @@ namespace angel
 class NoReordering
 {
 public:
-  using order = std::vector<uint32_t>;
+    using order = std::vector<uint32_t>;
 
 public:
-  explicit NoReordering()
-  {
-  }
-
-  std::vector<order> run( uint32_t num_vars ) const
-  {
-    order current_order;
-    for ( auto i = 0u; i < num_vars; ++i )
+    explicit NoReordering()
     {
-      current_order.emplace_back( i );
     }
-    return { current_order };
-  }
+
+    std::vector<order> run(uint32_t num_vars) const
+    {
+        order current_order;
+        for (auto i = 0u; i < num_vars; ++i)
+        {
+            current_order.emplace_back(i);
+            std::reverse(current_order.begin(), current_order.end());
+        }
+        return {current_order};
+    }
 };
 
-} /// namespace angel end
+} // namespace angel
