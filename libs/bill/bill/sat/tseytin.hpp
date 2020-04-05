4ea3f9559ae8af784085cd5211f1f4ad949b7bbb
Heinz Riener <heinz.riener@gmail.com>
2020-04-05 17:35:50 +0200

    bill: add_xor_clause.

--- libs/bill/bill/sat/tseytin.hpp
+++ libs/bill/bill/sat/tseytin.hpp
@@ -5,6 +5,8 @@
 #pragma once
 
 #include "interface/types.hpp"
+#include <vector>
+#include <queue>
 
 namespace bill {
 
@@ -74,6 +76,40 @@ lit_type add_tseytin_equals(Solver& solver, lit_type const& a, lit_type const& b
 	solver.add_clause(std::vector{a, ~b, lit_type(r, lit_type::polarities::negative)});
 	solver.add_clause(std::vector{a, b, lit_type(r, lit_type::polarities::positive)});
 	return lit_type(r, lit_type::polarities::positive);
+}
+
+template<typename Solver>
+lit_type add_xor_clause(Solver& solver, std::vector<lit_type> const& clause, lit_type::polarities pol = lit_type::polarities::positive )
+{
+  std::queue<lit_type> lits;
+  bool first = pol == lit_type::polarities::negative;
+  for ( const auto& l : clause )
+  {
+    if ( first )
+    {
+      lits.push( ~l );
+      first = false;
+    }
+    else
+    {
+      lits.push( l );
+    }
+  }
+
+  while ( lits.size() > 1 )
+  {
+    auto const a = lits.front();
+    lits.pop();
+    auto const b = lits.front();
+    lits.pop();
+
+    lits.push( add_tseytin_xor( solver, a, b ) );
+  }
+
+  assert( lits.size() == 1u );
+  solver.add_clause( { lits.front() } );
+
+  return lits.front();
 }
 
 } /* namespace bill */
