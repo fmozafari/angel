4e1fc34faba082de92cfaa35602f387eebd5d79c
Heinz Riener <heinz.riener@gmail.com>
2021-03-16 13:00:33 +0100

    fix some tweedledum issues.

--- libs/tweedledee/tweedledee/qasm/ast/ast_node_kinds.hpp
+++ libs/tweedledee/tweedledee/qasm/ast/ast_node_kinds.hpp
@@ -16,7 +16,7 @@ enum class ast_node_kinds : unsigned short {
 	#include "ast_node_kinds.def"
 };
 
-static const char * const ast_node_names[] = {
+inline const char * const ast_node_names[] = {
 	#define AST_NODE(X) #X,
 	#include "ast_node_kinds.def"
 	nullptr
@@ -24,7 +24,7 @@ static const char * const ast_node_names[] = {
 
 // Determines the name of a ast node as used within the front end.
 // The name of a ast node will be an internal name.
-const char *ast_node_name(ast_node_kinds k)
+inline const char *ast_node_name(ast_node_kinds k)
 {
 	auto k_idx = static_cast<int>(k);
 	return ast_node_names[k_idx];
