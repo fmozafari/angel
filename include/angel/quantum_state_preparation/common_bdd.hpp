7a31610a32e1786056b9adc7dd002a9c650895a1
Fereshte <fereshteh.mozafari88@gmail.com>
2021-11-10 16:20:09 +0100

    first draft for sparse QSP using DD

---
+++ include/angel/quantum_state_preparation/common_bdd.hpp
@@ -0,0 +1,130 @@
+
+
+namespace angel
+{
+
+struct create_bdd_param
+{
+  enum class strategy : uint32_t
+  {
+    create_from_tt,
+    create_from_pla
+  } strategy = strategy::create_from_tt;
+};
+
+BDD create_bdd_from_pla( Cudd& cudd, std::string file_name, uint32_t& num_inputs )
+{
+  std::ifstream infile( file_name );
+  std::string in, out;
+  infile >> in >> out;
+  num_inputs = std::atoi( out.c_str() );
+  BDD output;
+  auto bddNodes = new BDD[num_inputs];
+  for ( int i = num_inputs - 1; i >= 0; i-- )
+  {
+    bddNodes[i] = cudd.bddVar(); // index 0: LSB
+  }
+
+  bool sig = 1;
+  while ( infile >> in >> out )
+  {
+    BDD tmp, var;
+    bool sig2 = 1;
+
+    for ( auto i = 0u; i < num_inputs; ++i )
+    {
+      if ( in[i] == '-' )
+        continue;
+      var = bddNodes[i];
+
+      if ( in[i] == '0' )
+        var = !var;
+      if ( sig2 )
+      {
+        tmp = var;
+        sig2 = 0;
+      }
+      else
+        tmp &= var;
+    }
+
+    if ( sig )
+    {
+      output = tmp;
+      sig = 0;
+    }
+    else
+      output |= tmp;
+  }
+  return output;
+}
+
+BDD create_bdd_from_tt_str( Cudd& cudd, std::string tt_str, uint32_t num_inputs )
+{
+  auto bddNodes = new BDD[num_inputs];
+  for ( int i = num_inputs - 1; i >= 0; i-- )
+  {
+    bddNodes[i] = cudd.bddVar(); // index 0: LSB
+  }
+
+  BDD f_bdd;
+  int sig = 1;
+
+  /* 
+    zero index in tt_str consist the biggest minterm of tt 
+  */
+
+  for ( int32_t i = tt_str.size()-1; i >= 0; i-- )
+  {
+    if ( tt_str[i] == '1' )
+    {
+      auto n = (tt_str.size()-1) - i;
+      BDD temp;
+      temp = ( ( n & 1 ) == 1 ) ? bddNodes[0] : !bddNodes[0];
+      n >>= 1;
+      for ( auto j = 1u; j < num_inputs; j++ )
+      {
+        temp &= ( ( n & 1 ) == 1 ) ? bddNodes[j] : !bddNodes[j];
+        n >>= 1;
+      }
+
+      if ( sig )
+      {
+        f_bdd = temp;
+        sig = 0;
+      }
+      else
+      {
+        f_bdd |= temp;
+      }
+    }
+  }
+
+  return f_bdd;
+}
+
+BDD create_bdd( Cudd& cudd, std::string str, create_bdd_param bdd_param, uint32_t& num_inputs )
+{
+  BDD bdd;
+  if ( bdd_param.strategy == create_bdd_param::strategy::create_from_tt )
+    bdd = create_bdd_from_tt_str( cudd, str, num_inputs );
+  else
+    bdd = create_bdd_from_pla( cudd, str, num_inputs );
+
+  return bdd;
+}
+
+void draw_dump( DdNode* f_add, DdManager* mgr )
+{
+  /* command to run: dot -Tpng graph.dot > output.png */
+  FILE* outfile; /* output file pointer for .dot file */
+  outfile = fopen( "graph.dot", "w" );
+  DdNode** ddnodearray = (DdNode**)malloc( sizeof( DdNode* ) ); /* initialize the function array */
+  ddnodearray[0] = f_add;
+  Cudd_DumpDot( mgr, 1, ddnodearray, NULL, NULL, outfile ); /* dump the function to .dot file */
+}
+
+
+
+
+} //namespace angel end
